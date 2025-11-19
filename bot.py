30697dca1c84fb8862c97408eab9a82d504d63d7
Szymon Kiklica <kiklicaszymon@gmail.com>
2025-11-19 19:23:54 +0100

    new functionality
    adding a new bot functionality, sending messages every hour

--- bot.py
+++ bot.py
@@ -1,17 +1,17 @@
 import discord
 import asyncio
-
 import os
-TOKEN = os.getenv("TOKEN") # Wklej tu token swojego bota
+from datetime import datetime
+
+TOKEN = os.getenv("TOKEN")  # Twój token bota
 CHANNEL_ID = 1440757951689392300  # ID kanału, gdzie bot ma wysyłać wiadomości
-MESSAGE = "test"  # Treść wiadomości
-INTERVAL = 10  # Czas w sekundach (np. 60 = 1 minuta)
+INTERVAL = 3600  # co godzinę (3600 sekund)
 
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-async def send_message_periodically():
+async def send_time_hourly():
     await client.wait_until_ready()
     channel = client.get_channel(CHANNEL_ID)
     if channel is None:
@@ -19,13 +19,15 @@ async def send_message_periodically():
         return
 
     while True:
-        await channel.send(MESSAGE)
+        current_hour = datetime.now().hour  # pobranie aktualnej godziny
+        message = f"⏰ Jest godzina {current_hour}:00"
+        await channel.send(message)
         await asyncio.sleep(INTERVAL)
 
 @client.event
 async def on_ready():
     print(f"✔️ Zalogowano jako: {client.user}")
-    # Tu startujemy zadanie
-    asyncio.create_task(send_message_periodically())
+    # Uruchomienie zadania
+    asyncio.create_task(send_time_hourly())
 
 client.run(TOKEN)
